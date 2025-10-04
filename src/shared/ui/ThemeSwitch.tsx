b0d40dc1b539253e4114fa35e0b863b872a74efb
Vladimir Nehoc <vladimir.n@viletech.pro>
2025-10-04 20:17:55 +0400

    fix theme switcher

--- src/shared/ui/ThemeSwitch.tsx
+++ src/shared/ui/ThemeSwitch.tsx
@@ -3,25 +3,15 @@ import { useEffect, useState } from "react";
 const THEME_KEY = "theme";
 
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-
-  // при монтировании читаем localStorage и системные настройки
-  useEffect(() => {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
     const saved = localStorage.getItem(THEME_KEY) as "light" | "dark" | null;
-    if (saved) {
-      setTheme(saved);
-      document.documentElement.setAttribute("data-theme", saved);
-    } else {
-      const prefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
-      const init = prefersDark ? "dark" : "light";
-      setTheme(init);
-      document.documentElement.setAttribute("data-theme", init);
-    }
-  }, []);
+    if (saved) return saved;
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    return prefersDark ? "dark" : "light";
+  });
 
-  // обновляем тему при изменении
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem(THEME_KEY, theme);
@@ -31,17 +21,17 @@ export default function ThemeSwitch() {
     <button
       onClick={() => setTheme((prev) => (prev === "light" ? "dark" : "light"))}
       className={`
-    relative w-10 h-6 flex items-center rounded-full p-1 transition-colors duration-300
-    ${theme === "dark" ? "bg-base-200" : "bg-neutral-300"}
-  `}
+        relative w-10 h-6 flex items-center rounded-full p-1 transition-colors duration-300
+        ${theme === "dark" ? "bg-base-200" : "bg-neutral-300"}
+      `}
     >
       <span
         className={`
-      w-4 h-4 ${
-        theme === "dark" ? "bg-stone-400" : "bg-neutral-400"
-      } rounded-full transform transition-transform duration-300
-      ${theme === "dark" ? "translate-x-4" : "translate-x-0"}
-    `}
+          w-4 h-4 ${
+            theme === "dark" ? "bg-stone-400" : "bg-neutral-400"
+          } rounded-full transform transition-transform duration-300
+          ${theme === "dark" ? "translate-x-4" : "translate-x-0"}
+        `}
       />
     </button>
   );
