589d9388f9eca6b1d062619b480ca5816c6be0bc
Vladimir Nehoc <vladimir.n@viletech.pro>
2025-11-11 23:11:16 +0400

    add background preload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import { AppRoutes } from "./app/routes";
 import AppLayout from "@/shared/layouts/AppLayout";
+import PreloadBackgrounds from "./shared/PreloadBackgrounds";
 
 export default function App() {
   return (
     <AppLayout>
       <AppRoutes />
       <Toaster position="top-right" />
+      <PreloadBackgrounds />
     </AppLayout>
   );
 }
