d1a732eb195f16fc22ab39cbe47d37b1418f2db3
Vladimir Nehoc <vladimir.n@viletech.pro>
2025-11-15 03:00:16 +0400

    theme optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { AppRoutes } from "./app/routes";
 import AppLayout from "@/shared/layouts/AppLayout";
-import PreloadBackgrounds from "./shared/PreloadBackgrounds";
 import GennioToaster from "./shared/ui/GennioToaster";
 
 import "@/shared/config/i18n";
@@ -10,7 +9,6 @@ export default function App() {
     <AppLayout>
       <AppRoutes />
       <GennioToaster />
-      <PreloadBackgrounds />
     </AppLayout>
   );
 }
