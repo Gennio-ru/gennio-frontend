2d415168f2fa63a4c1fb516c3417320bc6b37ef8
Vladimir Nehoc <vladimir.n@viletech.pro>
2025-12-09 16:52:48 +0400

    fix margins

--- src/pages/user/MainPage/index.tsx
+++ src/pages/user/MainPage/index.tsx
@@ -9,7 +9,7 @@ export default function MainPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full flex flex-col items-center mt-[70px] sm:mt-[120px]">
+    <div className="w-full flex flex-col items-center mt-[40px] sm:mt-[75px]">
       <AIGenerationTitle
         title="Gennio — ваш идеальный инструмент для творчества"
         description="Используйте готовые шаблоны, создавайте изображения с нуля или обрабатывайте фото по&nbsp;собственным промптам. Gennio превращает ваши слова и идеи в визуальные результаты с&nbsp;помощью&nbsp;ИИ"
